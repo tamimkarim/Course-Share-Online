7c0be7611cdafadaa85700642668f1d9d8771b81
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-09-20 19:00:01 +0600

    cart added into cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Button, Card, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import './Cart.css';
 
 const Cart = (props) => {
@@ -7,14 +10,28 @@ const Cart = (props) => {
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
         total = total + product.price;
-        
+
     }
 
     return (
         <div>
-            <h1>Order Summary</h1>
-            <h5>Added to cart:{cart.length} </h5>
-            <p>Total cost: {total}</p>
+                <Card>
+                    <Card.Header>
+                        <Nav variant="pills" defaultActiveKey="#first">
+                            <Nav.Item>
+                                <h3>Order Summary</h3>
+                                <Button className="align" variant="outline-info">  <FontAwesomeIcon icon={faShoppingCart} /> {cart.length}</Button>
+                            </Nav.Item> 
+                        </Nav>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Title>Total cost: ${total} </Card.Title>
+                        <Card.Text>
+                            With supporting text below as a natural lead-in to additional content.
+                        </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
         </div>
     );
 };
